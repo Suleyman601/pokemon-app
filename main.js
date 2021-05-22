301692fd4e59f93ec8814beaaab365a942f706d7
Suleyman601 <suleymanjama.32@gmail.com>
2021-05-22 15:58:38 -0700

    added random and search button

--- main.js
+++ main.js
@@ -8,39 +8,54 @@ let flavorTextElement = document.querySelector('#flavortxt');
 let imgElement = document.querySelector('img');
 let searchInputElement = document.querySelector('#search');
 let searchBtnElement = document.querySelector('#search-btn');
-
-
-fetch('https://pokeapi.co/api/v2/pokemon/tyranitar').then(function (response){
-  return response.json();
-}).then(function (data){
-  console.log(data);
-  let padding = "&nbsp&nbsp&nbsp&nbsp&nbsp";
-  let height = Math.round((data.height / 10) * 10)/10;
-  let weight = Math.round((data.weight/10) * 10)/10;
-  let name = data.name;
-  let type = data.types[0].type.name;
-  numElement.innerHTML += data.id;
-  nameElement.innerHTML += padding + name.charAt(0).toUpperCase() + name.slice(1);
-  typeElement.innerHTML += padding + type.charAt(0).toUpperCase() + type.slice(1);
-  heightElement.innerHTML += padding + height + " m";
-  weightElement.innerHTML += padding + weight + " kg";
-  imgElement.setAttribute('src', data.sprites.front_default)
-  fetch(data.species.url).then(function (response){
-    return response.json();
-  }).then(function (newData){
-    console.log(newData.flavor_text_entries.length);
+let randomBtnElement = document.querySelector('#random');
+function fetchData(pokeNameOrID){
+  let str = 'lucario';
+  if(pokeNameOrID !== ""){
+    str = pokeNameOrID;
+  }
     
-    let flavorTxt = "";
-    for (let index = 0; index < newData.flavor_text_entries.length; index++) {
-      const element = newData.flavor_text_entries[index];
-      if(element.language.name == "en" && element.flavor_text.length < 140){
-        flavorTxt = element.flavor_text;
-        break;
-      } 
-    }
-    flavorTextElement.innerHTML = flavorTxt.replace("", " ");
+  fetch(`https://pokeapi.co/api/v2/pokemon/${str}`).then(function (response){
+  return response.json();
+  }).then(function (data){
+    let height = Math.round((data.height / 10) * 10)/10;
+    let weight = Math.round((data.weight/10) * 10)/10;
+    let name = data.name;
+    let type = data.types[0].type.name;
+    numElement.innerHTML = "#" + data.id;
+    nameElement.innerHTML = name.charAt(0).toUpperCase() + name.slice(1);
+    typeElement.innerHTML = type.charAt(0).toUpperCase() + type.slice(1);
+    heightElement.innerHTML = height + " m";
+    weightElement.innerHTML = weight + " kg";
+    imgElement.setAttribute('src', data.sprites.front_default)
+    fetch(data.species.url).then(function (response){
+      return response.json();
+    }).then(function (newData){
+      console.log(newData.flavor_text_entries.length);
+      
+      let flavorTxt = "";
+      for (let index = 0; index < newData.flavor_text_entries.length; index++) {
+        const element = newData.flavor_text_entries[index];
+        if(element.language.name == "en" && element.flavor_text.length < 140){
+          flavorTxt = element.flavor_text;
+          break;
+        } 
+      }
+      flavorTextElement.innerHTML = flavorTxt.replace("", " ");
+    });
   });
-});
+}
+function btnEvent(e){
+  if(e.target.id == "search-btn"){
+    fetchData(searchInputElement.value);
+  }
+  else{
+    let randNum = Math.floor(Math.random() * 898) + 1;
+    fetchData(randNum);
+  }
+}
 
+document.onload = fetchData("");
 
-
+searchBtnElement.addEventListener('click', btnEvent);
+randomBtnElement.addEventListener('click', btnEvent);
